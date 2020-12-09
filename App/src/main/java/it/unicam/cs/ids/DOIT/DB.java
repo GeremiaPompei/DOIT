856d427c9ee822a7900f443d9fe075385b35f264
Daniele Baiocco <daniele.baiocco@studenti.unicam.it>
2020-12-09 11:32:22 +0100

    secondo

--- App/src/main/java/it/unicam/cs/ids/DOIT/DB.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.cs.ids.DOIT;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class DB {
-    public static final List<User> TOTAL_USERS = new ArrayList<>();
-    public static final List<PartecipationRequest> TOTAL_PAERTECIPATIONREQUEST = new ArrayList<>();
-}
--- App/src/main/java/it/unicam/cs/ids/DOIT/IRole.java
+++
@@ -1,8 +0,0 @@
-package it.unicam.cs.ids.DOIT;
-
-import java.util.List;
-
-public interface IRole {
-    void setUser(User user);
-    List<Project> getProjects();
-}

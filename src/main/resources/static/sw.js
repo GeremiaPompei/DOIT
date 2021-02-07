b6378bd43a85c29a656135e00e1fc68795551f26
Geremia <geremia.pompei@studenti.unicam.it>
2021-02-07 01:27:23 +0100

    deploy

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -4,9 +4,9 @@ self.addEventListener('install', function(event) {
             return cache.addAll([
                 'https://unpkg.com/vue/dist/vue.min.js',
                 'https://unpkg.com/vue-router/dist/vue-router.min.js',
-                /*'https://bootswatch.com/4/minty/bootstrap.css',
+                'https://bootswatch.com/4/minty/bootstrap.css',
                 'https://ielts.com.au/wp-content/uploads/2018/09/3_3200x2133.jpg',
-                '/',
+                /*'/',
                 '/index.html',
                 '/style.css',
                 '/manifest.json',

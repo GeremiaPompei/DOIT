14f8f1f88ecd3d6ba0a8b3d1f4c12a3449f4c77a
Geremia <geremia.pompei@studenti.unicam.it>
2021-02-12 12:55:04 +0100

    Personalizzazione per CUIA.

--- src/main/java/it/unicam/cs/ids/DOIT/DOITCommandLineRunner.java
+++ src/main/java/it/unicam/cs/ids/DOIT/DOITCommandLineRunner.java
@@ -21,11 +21,23 @@ public class DOITCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        projectStateRepository.save(new ProjectState(0L, "INITIAL", "Description..."));
-        projectStateRepository.save(new ProjectState(1L, "IN PROGRESS", "Description..."));
-        projectStateRepository.save(new ProjectState(2L, "TERMINAL", "Description..."));
-        categoryRepository.save(new Category("SPORT", "Description..."));
-        categoryRepository.save(new Category("INFORMATICA", "Description..."));
-        categoryRepository.save(new Category("CUCINA", "Description..."));
+        projectStateRepository.save(new ProjectState(0L, "INITIAL",
+                "Initial state."));
+        projectStateRepository.save(new ProjectState(1L, "IN PROGRESS",
+                "Evolution state."));
+        projectStateRepository.save(new ProjectState(2L, "TERMINAL",
+                "Terminal state."));
+        categoryRepository.save(new Category("WORKSHOP",
+                "Nel corso delle Giornate CUIA in Argentina e in Italia. Bando CUIA e relative collaborazioni con le reti UNIR e RUNDO."));
+        categoryRepository.save(new Category("PROGETTI DI RICERCA",
+                "In risposta ad altri bandi."));
+        categoryRepository.save(new Category("PROGETTI DI COOPERAZIONE BILATERALE",
+                "Italia Argentina in risposta a bandi."));
+        categoryRepository.save(new Category("ATTIVITA' FORMATIVA",
+                "Corsi di formazione, specializzazione, scuole estive o invernali."));
+        categoryRepository.save(new Category("DOUBLE DEGREE",
+                "Dottorati in co-tutela."));
+        categoryRepository.save(new Category("PUBBLICAZIONI CONGIUNTE",
+                ""));
     }
 }
